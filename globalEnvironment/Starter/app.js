1960ba1c5f98f078bca41825d257a2fd2cd354b5
Jordan Guy <jordan.guy.277@gmail.com>
2018-08-29 11:03:59 -0400

    Global Environment

---
+++ globalEnvironment/Starter/app.js
@@ -0,0 +1,13 @@
+//base execution context is the global context
+//creates a global object and 'this'
+
+//when the page is run JavaScript creates this global context
+
+//Global = not inside a function
+
+
+var a = 'hello world';
+
+function b() {
+    
+}
